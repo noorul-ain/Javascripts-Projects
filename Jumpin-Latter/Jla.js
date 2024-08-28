5010fbf41abe51abe25e35401b34b61b5cf96862
noorulain <89151770+noorul-ain@users.noreply.github.com>
2024-08-28 10:20:11 -0700

    Add files via upload

---
+++ Jumpin-Latter/Jla.js
@@ -0,0 +1,10 @@
+
+
+
+const container = document.querySelectorAll("span")
+
+container.forEach((item) =>{
+    item.addEventListener("click", () => {
+        item.classList.toggle("active") 
+      })
+})
